8cd2622b3dcac056beba0dbd8f04a10fbe0c9726
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-09-05 20:00:43 -0400

    Added some commenting.

--- standard_to_military.py
+++ standard_to_military.py
@@ -15,13 +15,13 @@ def time_conversion(s):
 print(time_conversion('12:43:20PM'))
 
 def time_conversion_v2(s):
-    if 'p' in s.lower():
-        if s[:2] == '12':
-            return s[:-2]
-        return str(int(s[0:2]) + 12) + (s[2:-2])
-    else:
-        if s[:2] == '12':
-            return '00' + (s[2:-2])
-        return s[:-2]
+    if 'p' in s.lower(): # If entered string has P as in PM, 
+        if s[:2] == '12': # Further, if the first 2 nums are 12, as in 12PM
+            return s[:-2] # Keep time format same, minus PM
+        return str(int(s[0:2]) + 12) + (s[2:-2]) # Otherwise, return 12 added to first two digits, concatenated with last digits, remove pm
+    
+    if s[:2] == '12': # If none of first case is true, and first digits are 12
+        return '00' + (s[2:-2]) # return 00 as first 2 digits, because AM, added to last digits, minus am.
+    return s[:-2] # Otherwise, digits all same, minus am.
 
 print(time_conversion_v2('12:00:01aM'))
