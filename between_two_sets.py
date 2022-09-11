cbd06c82744df3d09921b552de23cb1c50127b2c
George Grunfelder <68717396+GitGrunfelder@users.noreply.github.com>
2022-09-10 21:23:57 -0400

    Solved between two sets.

--- between_two_sets.py
+++ between_two_sets.py
@@ -1,31 +1,48 @@
-from itertools import cycle
+# from itertools import cycle
+from collections import Counter
+import math
 
-a = [2, 987]
-b = [16, 32, 96]
+a = [3, 4]
+b = [24, 48]
+all_prime_factors = []
+counts = {}
 
-largest = max(a)
-t_max = largest # Temp var to prevent overwriting.
-RUNNING = True
-while RUNNING: # While true
-    for num in cycle(a): # Cycle through array a, each item
-        while t_max % num != 0: # While current t_max value doesn't divide evenly by current num in array
-            t_max += largest # Increase temp max by original max value; Once this is no longer true:
-        if all(t_max % num == 0 for num in a) is True: # Check if all items are true in the statement
-            RUNNING = False # If so, set running to false, to end outer while loop.
-            break # Break out of for loop.
-print(t_max) # Give the value of the least common multiple.
+# Break down items in a to prime factors
+def prime_factorization(x):
+    prime_factors = [] # Add all values that are prime factors of x
+    divisor = 2 # Start at lowest prime number
+    while divisor <= x: # If x is 1, function complete.
+        if x % divisor == 0: # If x divided by current divisor leaves no remainder
+            prime_factors.append(divisor) # That divisor is a prime factor, add it to list.
+            x = x/divisor # number is updated to new value after dividing.
+        else:
+            divisor += 1 # If not divisible by 2, increase by 1, until next prime factor is hit.
+    return prime_factors
 
+# Not sure if I should include this in a function, just adds all factor lists to a bigger list.
+for num in a:
+    all_prime_factors.append(prime_factorization(num))
 
-# counter = 0
-# for num in range(a[0],b[0]+1):
-#     if any((num % i == 0 for i in a) and big % num == 0 for big in b):
+# This checks each list for a count of each item, assigns to dictionary, and if one factor list has a higher count,\
+# replaces that key with a higher value.
+def find_commons():
+    for list in all_prime_factors:
+            counted = Counter(list)
+            for i in counted:
+                if counted[i] > counts.get(i,0):
+                    counts[i] = counted[i]
+    return counts
+find_commons()
+# This creates a list of all factors to the power of # of times they appear most in any list.
+def find_lcm():
+    to_the_power = []
+    for factor, power in counts.items():
+        to_the_power.append(factor ** power)
+    lcm = math.prod(to_the_power) # math function gives product of new list.
+    return lcm
 
-#             counter+= 1
-#             print(num)
-# print(counter)
-
-
-
-
-
-
+counter = 0
+for num in range(find_lcm(), min(b)+1):
+    if all(num % i == 0 for i in a) and all(i % num == 0 for i in b) is True:
+        counter += 1
+print(counter)
